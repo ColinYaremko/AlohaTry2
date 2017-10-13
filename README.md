27a2112cd4fbc2bfdb49b407371284ef28bce6cd
Colin Yaremko <remta@shaw.ca>
2017-10-13 14:16:56 -0700

    added js button, flickity, smooth scroll

---
+++ README.md
@@ -0,0 +1,71 @@
+Aloha Apparel
+
+Project 1 for RED Academy
+
+Getting Started
+
+No special instructions required to get up and running.  Open from your computer.
+
+Prerequisites
+
+No installation required.  Hopefully works on most browsers
+
+Give examples
+Installing
+
+A step by step series of examples that tell you have to get a development env running
+
+Say what the step will be
+
+Give the example
+And repeat
+
+until finished
+End with an example of getting some data out of the system or using it for a little demo
+
+Running the tests
+
+Explain how to run the automated tests for this system
+
+Break down into end to end tests
+
+Explain what these tests test and why
+
+Give an example
+And coding style tests
+
+Explain what these tests test and why
+
+Give an example
+Deployment
+
+Add additional notes about how to deploy this on a live system
+
+Built With
+
+Visual Studio Code - The HTML, CSS Editor
+Chrome - Testing platform
+Flickity - Used to generate carousel
+
+Contributing
+
+Please read CONTRIBUTING.md for details on our code of conduct, and the process for submitting pull requests to us.
+
+Versioning
+
+We use SemVer for versioning. For the versions available, see the tags on this repository.
+
+Authors
+
+Colin Yaremko - Initial work
+See also the list of contributors who participated in this project.
+
+License
+
+No license presently required
+
+Acknowledgments
+
+Hat tip to anyone who's code was used
+Inspiration
+etc
---
+++ scripts.js/write.js
@@ -0,0 +1,82 @@
+//alert('Hello World!');
+
+
+$('.main-carousel').flickity({
+  // options
+  cellAlign: 'left',
+  contain: true,
+  cellSelector: '.carousel-cell',
+  prevNextButtons: false,
+  autoPlay: true,
+  groupCells: 1
+});
+
+
+
+/*https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_eff_animate_smoothscroll
+*/
+ $(document).ready(function () {
+    // Add smooth scrolling to all links
+    $("a").on('click', function (event) {
+
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+
+        // Store hash
+        var hash = this.hash;
+
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function () {
+
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      } // End if
+    });
+  }); //Smooth scroll tested and working presently.
+
+
+
+  var inputText,
+      successStatus;
+
+  $('.sbuttton button').on('click', function(){
+    
+    inputText = $('#subscribe-email').val();
+      // console.log(inputText);
+    });
+
+    $.validator.setDefaults({
+      debug: false
+    });
+
+    // https://jqueryvalidation.org/documentation/
+    $( "#subscribe-form" ).validate({
+      rules: {
+        field: {
+          required: true,
+          email: true
+        }
+      },
+      success: function() {
+        successStatus = "yes";
+      //  console.log('yes');
+      },
+      submitHandler: function() { 
+        if(successStatus == "yes"){
+          alert("Submitted! " + inputText);
+        } 
+        else {
+          alert('Why no email');
+        }
+       
+      }
+    });
+
+
+//alert('Good bye world!');
